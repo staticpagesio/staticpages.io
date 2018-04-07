9ec0a0e7ebc41e0800b5e554521fbb9e1d9d232a
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-04-06 22:08:27 -0700

    Move 'balloon', 'jquery.validate', 'additional-methods' and 'custom' from the current page

---
+++ _assets/js/custom.js
@@ -0,0 +1,32 @@
+// custom extension for jQuery (https://stackoverflow.com/a/920322)
+jQuery.fn.exists = function () {
+  return this.length !== 0;
+};
+
+(function(global, $) {
+  "use strict";
+
+  // port of email validation regex from Ruby on Rails - /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  // setup validation for the only form
+  $("#hey-iam-in-form").validate({
+    // display errors only on form submission
+    onkeyup: false,
+    onfocusout: false,
+    onclick: false,
+    // customize error label container to display validation errors
+    errorLabelContainer: "#omg-validation-errors",
+    showErrors: function(errorMap, errorList) {
+      // apply data-balloon-* attributes to the parent element
+      for(var i in errorList) {
+        // here is our error object
+        var error = errorList[i];
+        // obtain reference to the parent
+        var parent = $(error.element).parent();
+        // apply "data-balloon" attributes to display tooltip
+        parent
+          .attr("data-balloon", error.message)
+          .attr("data-balloon-visible", ""); 
+      }
+    }
+  });
+})(this, jQuery);
