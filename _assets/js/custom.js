76e45e0f4724dee90673002f1f08a05dbc09e2d6
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-04-21 00:02:02 -0700

    Finally, all things seem to be in shape and we are ready to ship

--- _assets/js/custom.js
+++ _assets/js/custom.js
@@ -6,24 +6,31 @@ jQuery.fn.exists = function () {
 (function(global, $) {
   "use strict";
 
+  function dismissBalloon() {
+    // dismiss hint (if any)
+    $("*[data-balloon-visible]").removeAttr("data-balloon-visible data-balloon");
+  }
+
   // handle modal dismissal
   $("#count-me-in").on("hidden.bs.modal", function() {
-    // dismiss hint (if any)
-    $("*[data-balloon-visible]", this)
-      .removeAttr("data-balloon-visible data-balloon");
+    dismissBalloon();
     // clear input form
     $(":input", this).val("");
   });
 
   // port of email validation regex from Ruby on Rails - /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-  // setup validation for the only form
-  $("#hey-iam-in-form").validate({
-    // display errors only on form submission
+  // prepare validation settings
+  var validationSettings = {
+    // NOTE: This setup displays errors only on form submission
     onkeyup: false,
-    onfocusout: false,
     onclick: false,
-    // customize error label container to display validation errors
-    errorLabelContainer: "#omg-validation-errors",
+    // NOTE: Do not auto-focus on invalid elements, otherwise
+    // validation brings-in a glitch where the invalid element
+    // misses the next click.
+    focusInvalid: false,
+    // Dismiss hint balloon when element loses focus
+    onfocusout: function() { dismissBalloon() },
+    // Shows actual validation errors using balloon.css
     showErrors: function(errorMap, errorList) {
       // apply data-balloon-* attributes to the parent element
       for(var i in errorList) {
@@ -37,5 +44,11 @@ jQuery.fn.exists = function () {
           .attr("data-balloon-visible", ""); 
       }
     }
-  });
+  };
+  // NOTE: Unfortunately jQuery Validation does not support multiple form selectors
+  // therefore we fallback to shared validation settings + per form setup.
+  // https://jqueryvalidation.org/reference#link-validating-multiple-forms-on-one-page
+  $("#hey-iam-in-form, #insiders-squad-form").each(function() {
+    $(this).validate(validationSettings);
+  })
 })(this, jQuery);
